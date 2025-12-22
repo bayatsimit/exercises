bc25b9227e8c01a6e997d12201f124d30e99480c
bayatsimit <aslicihantimur@gmail.com>
2025-12-23 00:01:49 +0100

    screen recorder without time limitation

--- screcorder.py
+++ screcorder.py
@@ -1,21 +1,23 @@
+import os
+import keyboard
 import cv2
 import numpy as np
 import pyautogui #control mouse and keyboard
 
 scrSize=tuple(pyautogui.size()) #get the screen size and convert it to a tuple firom size pbj
 fourcc=cv2.VideoWriter_fourcc(*"XVID") 
-fps=12
 
-out=cv2.VideoWriter(f"output.avi", fourcc, fps, (scrSize))
-reqtime=5
+count=0
+while os.path.exists(f"output_{count}.avi"): #dont overwrite
+    count+=1
+out=cv2.VideoWriter(f"output_{count}.avi", fourcc, 12, (scrSize)) #fps=12
 
-for i in range(reqtime*fps): #for each frame
+while True:
     ss=pyautogui.screenshot() #take a ss
     frame=cv2.cvtColor(np.array(ss), cv2.COLOR_BGR2RGB) #convert ss to np array and rgb
-    out.write(frame)
-    cv2.imshow("screenshoot", frame)
-    if cv2.waitKey(1)==ord("q"):
+    out.write(frame) 
+
+    if keyboard.is_pressed("q"):
         break
 
-cv2.destroyAllWindows()
 out.release()
